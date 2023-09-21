fb86148b7d40932b132453c57d63e954ddc28686
Ivan Zatravkin <vzatravkin@gmail.com>
2023-09-21 14:15:42 +0400

    WIP: physics

--- src/scenes/demo.tsx
+++ src/scenes/demo.tsx
@@ -1,10 +1,22 @@
-import { Scene } from "react-babylonjs";
-import { Vector3 } from "@babylonjs/core";
+import {
+    GroundMesh,
+    Mesh,
+    PhysicsAggregate,
+    PhysicsShapeType,
+    Vector3,
+} from "@babylonjs/core";
+import { useEffect, useRef } from "react";
+import { useScene } from "react-babylonjs";
 import { Arc } from "models/arc";
 
 const DemoScene = () => {
+    const groundRef = useRef<GroundMesh>(null);
+    usePhysicsBody(groundRef, PhysicsShapeType.BOX, {
+        mass: 0,
+        restitution: 0.9,
+    });
   return (
-    <Scene>
+    <>
       <arcRotateCamera
         name="camera1"
         alpha={Math.PI / 2}
@@ -18,20 +30,55 @@ const DemoScene = () => {
         intensity={0.7}
         direction={Vector3.Up()}
       />
-      <Arc
-        position={new Vector3(12.5, -5, 0)}
-        scale={new Vector3(2, 2, 2)}
-        name="arc1"
-      />
-      <Arc
-        position={new Vector3(0, 0, 0)}
-        scale={new Vector3(1, 1, 1)}
-        name="arc2"
-      />
-        <ground name="ground" width={6} height={6} />
+        <Arc
+            position={new Vector3(0, 0, 0)}
+            scale={new Vector3(1, 1, 1)}
+            name="arc2"
+        />
+        <BouncyBox />
+    <ground name="ground" width={6} height={6}>
+    </ground>
+  </>
+);
+};
 
-    </Scene>
-  );
+const BouncyBox = () => {
+    const boxRef = useRef<Mesh>(null);
+    usePhysicsBody(boxRef, PhysicsShapeType.BOX, {
+        mass: 1,
+        restitution: 0.9,
+    });
+
+    return (
+        <box name="box" size={1} position={new Vector3(0, 5, 0)} ref={boxRef} />
+    );
+};
+
+/**
+ * usePhysicsBody hook adds a physics body to a mesh
+ */
+export const usePhysicsBody = (
+  mesh: React.MutableRefObject<Mesh | null>,
+  shapeType: PhysicsShapeType,
+  options: {
+    mass: number;
+    restitution: number;
+  },
+) => {
+  const scene = useScene();
+  useEffect(() => {
+    //connect box to physics engine
+    if (!mesh.current || !scene) return;
+    const boxAggregate = new PhysicsAggregate(
+      mesh.current,
+      shapeType,
+      options,
+      scene,
+    );
+    return () => {
+      boxAggregate.dispose();
+    };
+  }, []);
 };
 
 export default DemoScene;
